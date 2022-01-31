e3125c4e30ca5457a854799a901994786dec5b47
paulacarvani <3312@holbertonschool.com>
2022-01-31 17:22:23 -0500

    Task 0, correction of format in first line

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -33,8 +33,8 @@ if __name__ == "__main__":
 
     """Prints info of user"""
     print("Employee {} is done with task({}/{}:)".format(us_name,
-                                                        task_comp,
-                                                        task_all))
+                                                         task_comp,
+                                                         task_all))
 
     """Prints info of todos"""
     [print("\t {}".format(task)) for task in desc_task]
