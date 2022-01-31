43ad8a386932a630bf24f9d61668972cdfa88e16
paulacarvani <3312@holbertonschool.com>
2022-01-31 17:33:47 -0500

    Change style

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" Python script that, using a REST API in url,
+""" 
+Python script that, using a REST API in url,
 for a given employee ID, returns information about his/her TODO list progress
 """
 
