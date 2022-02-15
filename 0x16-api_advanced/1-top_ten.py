9b51142228232f3cee72cafad19d2f1932539475
paulacarvani <3312@holbertonschool.com>
2022-02-15 13:56:05 -0500

    Task 1, deleted import not used

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """Function that queries the Reddit API and prints the
 titles of the first 10 hot posts listed for a given subreddit."""
-from multiprocessing.sharedctypes import Value
-from tkinter.messagebox import NO
 from requests import get
 
 
